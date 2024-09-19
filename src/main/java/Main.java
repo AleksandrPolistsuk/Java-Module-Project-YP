9c2f8a627cf27d6284db28912bb024d5283343bd
lucky <luckycampshop@gmail.com>
2024-09-19 13:21:18 +0300

    Добавлена проверка на ошибку ввода строки скорости автомобиля. Теперь при вводе букв или не целого числа, выводится сообщение об ошибке и просит новый ввод.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,5 @@
 import java.util.ArrayList;
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
       CarRace carRaces = new CarRace();
@@ -8,18 +7,14 @@ public class Main {
       carRaces.displayWinner();
     }
 }
-
 class CarRace {
     Scanner scanner = new Scanner(System.in);
     ArrayList <Car> carList = new ArrayList<>();
-
     String leadingCar = ""; // строка для победителя
     int maxDistance = 0; // строка для пройденой дистанции
-
     // Метод расчета дистанции и сохранения лучших данных
     public void distance(String nameCar, int speedCar) {
         int distance = speedCar * 24;
-
         if (distance > maxDistance) {
             leadingCar = nameCar;
             maxDistance = distance;
@@ -33,42 +28,40 @@ class CarRace {
             System.out.println("Нет данных о победителе.");
         }
     }
-
     // метод сбора данных и передачи их
 public void start () {
     System.out.println("Добро пожаловать на гонку 24 часа Ле-Мана!");
     System.out.println("Для начала гонки, введите название и скорость 3 автомобилей.");
-
-
     for (int i=1;i<=3;i++){
         System.out.println("Введите название автомобиля номер " +i +":");
         String nameCar = scanner.nextLine();
         int speedCar;
- while (true){
-     System.out.println("Введите скорость (от 0 до 250) автомобиля номер " +i +":");
-     speedCar = Integer.parseInt(scanner.nextLine());
-     if (speedCar >= 0 && speedCar <= 250) {
-         break;
-        } else {
-            System.out.println("Скорость должна быть в диапазоне от 0 до 250. Попробуйте снова.");
+        while (true) {
+            System.out.println("Введите скорость (от 0 до 250) автомобиля номер " + i + ":");
+            String input = scanner.nextLine();  // считываем ввод как строку
 
-        }}
+            try {
+                               speedCar = Integer.parseInt(input);
 
+                if (speedCar >= 0 && speedCar <= 250) {
+                    break;
+                } else {
+                    System.out.println("Скорость должна быть в диапазоне от 0 до 250. Попробуйте снова.");
+                }
+            } catch (NumberFormatException e) {
+              System.out.println("Скорость введена некорректно. Пожалуйста, введите целое число от 0 до 250.");
+            }
+        }
         carList.add(new Car(nameCar, speedCar));
         distance(nameCar, speedCar);
     }
-
    scanner.close();
-
 }
 }
-
-
 // обьект Car, который имеет название и скорость.
 class Car {
     String name;
     int speed;
-
     public Car(String name, int speed) {
         this.name = name;
         this.speed = speed;
